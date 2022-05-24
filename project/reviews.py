7e9d298b46a124a9c680e3879e7cd98d59ddeacd
Sumit Dey <sumitdey0007@gmail.com>
2022-05-24 15:42:14 +0200

    Man kan søge efter film og tilføje en review

--- project/reviews.py
+++ project/reviews.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, url_for, redirect, g
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 import os
+
 from neo4j import (
     GraphDatabase,
     basic_auth,
@@ -25,38 +26,36 @@ port = os.getenv("PORT", 8080)
 
 driver = GraphDatabase.driver(NEO4J_URI, auth=basic_auth(NEO4J_USER, NEO4J_PASSWORD))
 
-def get_movies(driver):
+def get_movies(driver, movie):
     all_movies = []
     session = driver.session()
-    for record in session.run("MATCH(m:Movie) return m.title LIMIT 10"):
+    for record in session.run("MATCH (m:Movie) WHERE toLower(m.title) CONTAINS toLower($title) RETURN m.title", {"title": movie}):
         all_movies.append(record["m.title"])
     return all_movies
 
 @app.route('/', methods=('GET', 'POST'))
 def write_review():
+    all_reviews = reviews.find()
 
-    all_movies = get_movies(driver)
-    my_id = request.form.get("movie","")
-    print (my_id)
-    # if request.method == "POST" and request.:
-    #     print(request.form['movie'])
+    if request.method=='POST' and 'movie' in request.form:
+        movie = request.form.get("movie")
+        searched_movies = get_movies(driver, movie)
+        return render_template('reviews.html', movies = searched_movies)
 
-    if request.method=='POST':
+    if request.method=='POST' and "review_text" in request.form:
         review = request.form['review_text']
         movie_name = request.form['movie_name']
         rating = request.form['rating']
         date = datetime.now().strftime("%x")
         reviews.insert_one({'review': review, 'name': movie_name, 'date': date, 'rating': rating})
-        return redirect(url_for('index'))
-
-    all_reviews = reviews.find()
+        return redirect(url_for('write_review'))
    
-    return render_template('index.html', movies = all_movies)
+    return render_template('reviews.html', reviews = all_reviews)
 
 @app.route('/<id>/delete/')
 def delete(id):
     reviews.delete_one({"_id": ObjectId(id)})
-    return redirect(url_for('index'))
+    return redirect(url_for('write_review'))
 
 if __name__ == "__main__":
     app.run(debug=True, port=5002)
