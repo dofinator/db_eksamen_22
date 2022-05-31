537aa049a63e6a59b1dd0d99a74865e236d1de5a
= <cph-cw109@cphbusiness.dk>
2022-05-31 17:18:28 +0200

    updated sql

---
+++ project/settings.py
@@ -0,0 +1,27 @@
+CONNECTION_POSTGRES = """ 
+host = 'localhost'
+port = '5432'
+user = 'postgres'
+dbname = 'postgres'
+password = 'postgres'
+"""
+
+SECRET = "change to secret key"
+
+
+NEO4J_URI="neo4j://localhost:7687 "
+NEO4J_DATABASE="neo4j" 
+NEO4J_USER="neo4j" 
+NEO4J_PASSWORD="password"
+
+# Neo4j
+# user: neo4j
+# password : password
+
+# Postgres
+# user: postgres
+# password : postgres
+
+# Mongodb
+# user: root
+# password : rootpassword
