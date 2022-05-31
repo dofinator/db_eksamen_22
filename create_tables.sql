e2867b89c242942381fc001cbc2addf4c8b7f453
= <cph-cw109@cphbusiness.dk>
2022-05-31 11:02:30 +0200

    update sql file

--- create_tables.sql
+++ create_tables.sql
@@ -1,24 +1,35 @@
 begin;
+--abort;
+drop table if exists error_log;
+drop table if exists users; 
+drop table if exists roles;
 
-drop table error_log;
-drop table users; 
+
 --------------------------------------------------------
 
 -- Create tables
 
 create table users(
 	id serial primary key,
-	email text UNIQUE,
+	email text unique,
 	password text,
 	name text,
-	created_on timestamptz NULL DEFAULT now(),
+	role text default 'user',
+	admin_is_allowed boolean default false,
+	created_on timestamptz null default now(),
+	modified_on timestamptz null default now()
+);
+
+create table roles(
+	role text primary key,
+	created_on timestamptz null default now(),
 	modified_on timestamptz null default now()
 );
 
 
 create table error_log(
 	id serial primary key,
-	fk_user_id int,
+	fk_user_id int default null,
 	error text,
 	registered_on timestamptz null default now()
 );
@@ -28,8 +39,8 @@ create table error_log(
 
 -- Set foriegn keys
 
-ALTER TABLE public.error_log ADD CONSTRAINT fk_user_id FOREIGN KEY (fk_user_id) REFERENCES public.users(id);
-
+alter table public.error_log add constraint fk_user_id foreign key (fk_user_id) references public.users(id);
+alter table public.users add constraint fk_role foreign key (role) references public.roles(role);
 --------------------------------------------------------
 
 -- Trigger for when any of the users data is modified
@@ -49,13 +60,19 @@ before update on public.users
 FOR EACH row 
 EXECUTE PROCEDURE trigger_set_timestamptz_on();
 
+/* Trigger on the users table */
+CREATE TRIGGER set_timestamptz_modified_on
+before update on public.roles
+FOR EACH row 
+EXECUTE PROCEDURE trigger_set_timestamptz_on();
 
 --------------------------------------------------------
 
--- Insert test users
+-- Insert roles
+insert into public.roles (role) values ('user');
+insert into public.roles (role) values ('admin');
 
-insert into public.users (email,"password",name) values ('test@test.dk','test','test');
-
-insert into public.users (email,"password",name) values ('test2@test.dk','test2','test2');
+-- Insert admin
+insert into public.users (email,"password",name,role) values ('admin@admin.com','pbkdf2:sha256:260000$BF5lbo1SVwscE7BL$234537f55d246ae028b4a704c68189857ebabb7ae4b271e83232bd2c8b68b1fa','christoffer','admin');
 
 commit;
